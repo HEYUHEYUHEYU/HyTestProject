4e57ad6384124678b8741a94bd29f05240f08cd7
BLUECROSSHY <1849047053@qq.com>
2018-12-08 11:33:41 +0800

    Animal提交

---
+++ src/Animal/Cat.java
@@ -0,0 +1,43 @@
+package Animal;
+
+/**
+ * Created by heyu on 2018/12/8.
+ */
+public class Cat {
+    private String name;
+    private String color;
+    private Integer age;
+    private String gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
---
+++ src/Animal/Dog.java
@@ -0,0 +1,52 @@
+package Animal;
+
+/**
+ * Created by heyu on 2018/12/8.
+ */
+public class Dog {
+    private String name;
+    private String color;
+    private String size;
+    private Integer age;
+    private String gender;
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+}
